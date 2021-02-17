53fc8a0236967ff91f4c776aabe83f906dc91adb
Christina <christinavhastenrath@gmail.com>
2021-02-17 14:47:52 -0800

    adjusted plugin name and wording

--- wp-gh-actions.php
+++ wp-gh-actions.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Deploy plugin
+Plugin Name: Deploy to GitHub Actions
 Description: A deploy plugin to rerun github actions
 Author: Christina Hastenrath
 Version: 1.0
@@ -8,28 +8,31 @@ Version: 1.0
 add_action('admin_menu', 'test_plugin_setup_menu');
  
 function test_plugin_setup_menu(){
-        add_menu_page( 'Deploy Plugin Page', 'Deploy Plugin', 'manage_options', 'deploy-plugin', 'test_init' );
+        add_menu_page( 'Deploy Plugin Page', 'Deploy to GH Actions', 'manage_options', 'deploy-plugin', 'test_init' );
 }
  
 function test_init(){
 ?>
-	<h1>ReRun the Blog</h1>
-	<h2>Publish to Beta</h2>
+	<h1>Run GitHub Actions to deploy your Frontend</h1>
+
+	<h2>Publish to Beta Branch</h2>
 	<form method="post">
 		<input type="submit" class="button" name="button1" value="Beta"/>
 	</form>
 
-	<h2>Publish to Production</h2>
+	<h2>Publish to Production Branch</h2>
 	<form method="post">
 		<input type="submit" class="button" name="button2" value="Production"/>
 	</form>
-	<p>You can check the status of the publishing here <span><a href="https://github.com/<user name>/<repo>/actions">Github Actions</a></span></p>
+	<h3>You can check the GitHub Action status in your repo here <span><a href="https://github.com/<user name>/<repo>/actions">Github Actions</a></span></h3>
 
 <?php
 }
 
 if(isset($_POST['button1'])) {
-	echo "............................................The Beta Site is now rerunning and should be updated within a few minutes";
+	$title=".......................................The Beta Site GitHub Action is now running and your Beta Branch should be updated soon"; 
+	?><h2><?php echo $title; ?></h2><?php
+
 	$endpoint = 'https://api.github.com/repos/<username>/<repo>/dispatches';
 
 	$body = [
@@ -54,26 +57,28 @@ if(isset($_POST['button1'])) {
 }
 
 if(isset($_POST['button2'])) {
-	echo "............................................The Production Site is now rerunning and should     be updated within a few minutes";
-        $endpoint = 'https://api.github.com/repos/<username>/<repo>/dispatches';
-  
-        $body = [
-                'event_type' => 're-prod-deploy',
-        ];
-  
-        $body = wp_json_encode( $body );
-  
-        $options = [
-                'body'          => $body,
-                'headers'       => [
-                        'Content-Type' => 'application/json',
-                        'Accept' => 'application/vnd.github.everest-preview+json',
-                        'Authorization' => 'Bearer <your Bearer Token>'
-                ],
-                'httpversion' => '1.0',
-                'sslverify' => false,
-                'data_format' => 'body',
-        ];
+	$title=".......................................The Production Site GitHub Action is now running and your Production Branch should be updated soon"; 
+	?><h2><?php echo $title; ?></h2><?php
+
+	$endpoint = 'https://api.github.com/repos/<username>/<repo>/dispatches';
+
+	$body = [
+					'event_type' => 're-prod-deploy',
+	];
+
+	$body = wp_json_encode( $body );
+
+	$options = [
+					'body'          => $body,
+					'headers'       => [
+									'Content-Type' => 'application/json',
+									'Accept' => 'application/vnd.github.everest-preview+json',
+									'Authorization' => 'Bearer <your Bearer Token>'
+					],
+					'httpversion' => '1.0',
+					'sslverify' => false,
+					'data_format' => 'body',
+	];
   
 	wp_remote_post( $endpoint, $options );
 }
